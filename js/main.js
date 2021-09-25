24ad9029f80ce1dd3128b7d4d0e88cc48ecbc5c1
Kungfu-Chsia <allrighty@ya.ru>
2021-09-25 21:09:43 +0300

    задание про 2 функции

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var getRandomIntInclusive = function(min, max) {
+  if (max > min && min>=0 ) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else {
+    return "Некорректный диапазон чисел!";
+  }
+}
+
+var getRandomFloat = function(min, max, precision) {
+  if (max > min && min>=0 ) {
+    var number = Math.random() * (max - min) + min;
+    return number.toFixed(precision);
+  } else {
+    return "Некорректный диапазон чисел!"
+  }
+}
+
+console.log(getRandomIntInclusive(20, 30));
+console.log(getRandomFloat(10.3, 10.4, 5));
